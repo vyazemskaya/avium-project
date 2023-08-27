10a99fba953b2d2ca065bd1f53ee418bf3293fcf
vyazemskaya <114158560+vyazemskaya@users.noreply.github.com>
2023-08-28 02:08:08 +0300

    fix

--- js/animations.js
+++ js/animations.js
@@ -201,38 +201,203 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoWrap = videoSection.querySelector('.container_media')
     const md = window.matchMedia('(max-width: 768px)').matches
     const mmd = window.matchMedia('(min-width: 768px)').matches
+    const mm = gsap.matchMedia()
+    const tl1 = gsap.timeline()
+    const tl2 = gsap.timeline()
 
     if (window.matchMedia('(min-width: 768px)').matches) {
       video.play()
     }
-
     gsap.defaults({ duration: 1 })
     gsap.set(videoWrap, { opacity: 0, visibility: 'hidden' })
+    gsap.to(videoWrap, { opacity: 1, visibility: 'visible', delay: 2 })
 
-    const gsapSet = () => {
-      gsap.set(
-        videoWrap,
-        {
-          yPercent: md ? -23 : -17,
-          xPercent: md ? 38 : 5,
-          right: 0,
-          top: 0,
-          width: md ? '89.3rem' : '110.9rem',
-          height: md ? '89.3rem' : '110.9rem',
-          'border-radius': '50%',
-        },
-        0
-      )
-      gsap.set(
-        video,
-        {
-          width: md ? '67%' : '100%',
-          height: md ? '82%' : '100%',
-        },
-        0
-      )
-      gsap.set(closeBtn, { opacity: 0, visibility: 'hidden' }, 0)
-    }
+    gsap.set(
+      videoWrap,
+      {
+        yPercent: md ? -23 : -17,
+        xPercent: md ? 38 : 5,
+        right: 0,
+        top: 0,
+        width: md ? '89.3rem' : '110.9rem',
+        height: md ? '89.3rem' : '110.9rem',
+        'border-radius': '50%',
+      },
+      0
+    )
+    gsap.set(
+      video,
+      {
+        width: md ? '67%' : '100%',
+        height: md ? '82%' : '100%',
+      },
+      0
+    )
+    gsap.set(closeBtn, { opacity: 0, visibility: 'hidden' }, 0)
+
+    document.addEventListener('click', function (e) {
+      mm.add('(max-width: 768px)', () => {
+        if (
+          e.target.closest('.content_outer-btn') &&
+          !videoSection.classList.contains('_fw')
+        ) {
+          function getScreenOrientation() {
+            if (window.innerHeight < window.innerWidth) {
+              tl1.to(
+                videoWrap,
+                {
+                  width: '100vw',
+                  height: '100vh',
+                  rotate: 0,
+                  duration: 0,
+                  delay: 0,
+                },
+                0
+              )
+            }
+          }
+          getScreenOrientation()
+          window.addEventListener('resize', getScreenOrientation)
+
+          videoSection.classList.add('_fw')
+          tl2.kill()
+          tl1.to(closeBtn, { opacity: 1, visibility: 'visible' }, 0)
+          tl1.to(
+            video,
+            { width: '100%', height: '100%', duration: 0, delay: 0 },
+            0
+          )
+          tl1.to('header', { yPercent: -100, duration: 0, delay: 0 }, 0)
+          tl1.to('body', { overflow: 'hidden', duration: 0, delay: 0 }, 0)
+          tl1.to(
+            videoWrap,
+            {
+              position: 'fixed',
+              width: '100vh',
+              height: '100vw',
+              yPercent: -50,
+              rotate: 90,
+              xPercent: 50,
+              right: '50%',
+              top: '50%',
+              'border-radius': 0,
+              duration: 0,
+              delay: 0,
+            },
+            0
+          )
+          tl1.to(
+            playBtn,
+            {
+              opacity: 0,
+              visibility: 'hidden',
+              delay: 0,
+            },
+            0
+          )
+        } else if (
+          e.target.closest('.section_first #close-video') &&
+          videoSection.classList.contains('_fw')
+        ) {
+          videoSection.classList.remove('_fw')
+          tl1.kill()
+          tl2.to(
+            video,
+            {
+              width: '67%',
+              height: '82%',
+              duration: 0,
+              delay: 0,
+            },
+            0
+          )
+          tl2.to(
+            videoWrap,
+            {
+              position: 'absolute',
+              yPercent: -23,
+              xPercent: 38,
+              top: 0,
+              rotate: 0,
+              right: 0,
+              width: '89.3rem',
+              height: '89.3rem',
+              'border-radius': '50%',
+              duration: 0,
+              delay: 0,
+            },
+            0
+          )
+          tl2.to('header', { yPercent: 0, duration: 0, delay: 0 }, 0)
+          tl2.to('body', { overflow: 'visible', duration: 0, delay: 0 }, 0)
+          tl2.to(
+            playBtn,
+            {
+              opacity: 1,
+              visibility: 'visible',
+              delay: 0,
+            },
+            0
+          )
+        }
+      })
+      mm.add('(min-width: 768px)', () => {
+        if (
+          e.target.closest('.content_outer-btn') &&
+          !videoSection.classList.contains('_fw')
+        ) {
+          videoSection.classList.add('_fw')
+          tl2.kill()
+          tl1.to(
+            videoWrap,
+            {
+              width: '100%',
+              height: '100%',
+              yPercent: 0,
+              xPercent: 0,
+              'border-radius': 0,
+              delay: 0,
+            },
+            0
+          )
+          tl1.to(
+            playBtn,
+            {
+              opacity: 0,
+              visibility: 'hidden',
+              delay: 0,
+            },
+            0
+          )
+        } else if (videoSection.classList.contains('_fw')) {
+          tl1.kill()
+          tl2.to(
+            videoWrap,
+            {
+              yPercent: -17,
+              xPercent: 5,
+              width: '110.9rem',
+              height: '110.9rem',
+              'border-radius': '50%',
+              delay: 0,
+            },
+            0
+          )
+          tl2.to(
+            playBtn,
+            {
+              opacity: 1,
+              visibility: 'visible',
+              delay: 1,
+              onStart: () => {
+                videoSection.classList.remove('_fw')
+              },
+            },
+            0
+          )
+        }
+      })
+    })
 
     const addSourceToVideo = (element, src) => {
       const source = document.createElement('source')
@@ -248,181 +413,8 @@ document.addEventListener('DOMContentLoaded', function () {
         addSourceToVideo(element, src.dataset.mobileVid)
       }
     }
-    const gsapInit = () => {
-      gsap.to(videoWrap, { opacity: 1, visibility: 'visible', delay: 2 })
-      if (window.matchMedia('(max-width: 768px)').matches) {
-        document.addEventListener('click', function (e) {
-          const tl1 = gsap.timeline()
-          const tl2 = gsap.timeline()
-          if (
-            e.target.closest('.content_outer-btn') &&
-            !videoSection.classList.contains('_fw')
-          ) {
-            function getScreenOrientation() {
-              if (window.innerHeight < window.innerWidth) {
-                tl1.to(
-                  videoWrap,
-                  {
-                    width: '100vw',
-                    height: '100vh',
-                    rotate: 0,
-                    duration: 0,
-                    delay: 0,
-                  },
-                  0
-                )
-              }
-            }
-            getScreenOrientation()
-            window.addEventListener('resize', getScreenOrientation)
-
-            videoSection.classList.add('_fw')
-            tl2.kill()
-            tl1.to(closeBtn, { opacity: 1, visibility: 'visible' }, 0)
-            tl1.to(
-              video,
-              { width: '100%', height: '100%', duration: 0, delay: 0 },
-              0
-            )
-            tl1.to('header', { yPercent: -100, duration: 0, delay: 0 }, 0)
-            tl1.to('body', { overflow: 'hidden', duration: 0, delay: 0 }, 0)
-            tl1.to(
-              videoWrap,
-              {
-                position: 'fixed',
-                width: '100vh',
-                height: '100vw',
-                yPercent: -50,
-                rotate: 90,
-                xPercent: 50,
-                right: '50%',
-                top: '50%',
-                'border-radius': 0,
-                duration: 0,
-                delay: 0,
-              },
-              0
-            )
-            tl1.to(
-              playBtn,
-              {
-                opacity: 0,
-                visibility: 'hidden',
-                delay: 0,
-              },
-              0
-            )
-          } else if (
-            e.target.closest('.section_first #close-video') &&
-            videoSection.classList.contains('_fw')
-          ) {
-            videoSection.classList.remove('_fw')
-            tl1.kill()
-            tl2.to(
-              video,
-              {
-                width: '67%',
-                height: '82%',
-                duration: 0,
-                delay: 0,
-              },
-              0
-            )
-            tl2.to(
-              videoWrap,
-              {
-                position: 'absolute',
-                yPercent: -23,
-                xPercent: 38,
-                top: 0,
-                rotate: 0,
-                right: 0,
-                width: '89.3rem',
-                height: '89.3rem',
-                'border-radius': '50%',
-                duration: 0,
-                delay: 0,
-              },
-              0
-            )
-            tl2.to('header', { yPercent: 0, duration: 0, delay: 0 }, 0)
-            tl2.to('body', { overflow: 'visible', duration: 0, delay: 0 }, 0)
-            tl2.to(
-              playBtn,
-              {
-                opacity: 1,
-                visibility: 'visible',
-                delay: 0,
-              },
-              0
-            )
-          }
-        })
-      }
-      if (window.matchMedia('(min-width: 768px)').matches) {
-        document.addEventListener('click', function (e) {
-          const tl1 = gsap.timeline()
-          const tl2 = gsap.timeline()
-          if (
-            e.target.closest('.content_outer-btn') &&
-            !videoSection.classList.contains('_fw')
-          ) {
-            videoSection.classList.add('_fw')
-            tl2.kill()
-            tl1.to(
-              videoWrap,
-              {
-                width: '100%',
-                height: '100%',
-                yPercent: 0,
-                xPercent: 0,
-                'border-radius': 0,
-                delay: 0,
-              },
-              0
-            )
-            tl1.to(
-              playBtn,
-              {
-                opacity: 0,
-                visibility: 'hidden',
-                delay: 0,
-              },
-              0
-            )
-          } else if (videoSection.classList.contains('_fw')) {
-            tl1.kill()
-            tl2.to(
-              videoWrap,
-              {
-                yPercent: -17,
-                xPercent: 5,
-                width: '110.9rem',
-                height: '110.9rem',
-                'border-radius': '50%',
-                delay: 0,
-              },
-              0
-            )
-            tl2.to(
-              playBtn,
-              {
-                opacity: 1,
-                visibility: 'visible',
-                delay: 1,
-                onStart: () => {
-                  videoSection.classList.remove('_fw')
-                },
-              },
-              0
-            )
-          }
-        })
-      }
-    }
-
-    gsapSet()
-    gsapInit()
+    // gsapSet()
+    // gsapInit()
     initVideo(video, video)
   }
 
