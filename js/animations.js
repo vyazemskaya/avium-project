2413202be6a51bb7b8ae5bd1c141286ca5800f9a
vyazemskaya <114158560+vyazemskaya@users.noreply.github.com>
2023-08-28 14:42:52 +0300

    update

--- js/animations.js
+++ js/animations.js
@@ -871,3 +871,46 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 })
+
+const toggleBtn = document.querySelector('.rotate-icon')
+
+toggleBtn.addEventListener('click', function () {
+  openfullscreen()
+})
+
+function openfullscreen() {
+  // Trigger fullscreen
+  if (document.getElementById('parent').requestFullscreen) {
+    document.getElementById('parent').requestFullscreen()
+  } else if (document.getElementById('parent').mozRequestFullScreen) {
+    /* Firefox */
+    document.getElementById('parent').mozRequestFullScreen()
+  } else if (document.getElementById('parent').webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.getElementById('parent').webkitRequestFullscreen()
+  } else if (document.getElementById('parent').webkitRequestFullScreen) {
+    /* Chrome, Safari and Opera */
+    document.getElementById('parent').webkitRequestFullScreen()
+  } else if (document.getElementById('parent').msRequestFullscreen) {
+    /* IE/Edge */
+    document.getElementById('parent').msRequestFullscreen()
+  }
+}
+
+function closefullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen()
+  } else if (document.webkitExitFullScreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullScreen()
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen()
+  }
+}
